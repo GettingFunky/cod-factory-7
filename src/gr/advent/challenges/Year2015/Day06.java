606bf5b470348abe7c60c1b5abbcc2a46e15bc45
Ev. Theodorakis <sagemmyV75@yahoo.gr>
2025-01-18 09:17:41 +0200

    new advent 2015

---
+++ src/gr/advent/challenges/Year2015/Day06.java
@@ -0,0 +1,76 @@
+package gr.advent.challenges.Year2015;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public class Day06 {
+
+    public static void main(String[] args) {
+        String inFilePath = "E:\\Programming\\Advent Of Code\\2015\\Day06\\input.txt";
+
+        // String.split("[ ,]+"); // split on on one or more spaces or commas
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inFilePath))) {
+
+            String line;
+            boolean[][] lights = new boolean[1000][1000];
+//            String firstLine = reader.readLine();
+            String[] tokens;
+//            String[] headerTokens = firstLine.split(",");
+            int counterLightsOn = 0;
+
+            while ((line = reader.readLine()) != null) {
+                tokens = line.split("[ ,]+");
+                
+                if (Objects.equals(tokens[1], "on")) {
+                    for (int i = Math.min(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[5]));
+                         i <= Math.max(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[5])); i++) {
+                        for (int j = Math.min(Integer.parseInt(tokens[3]), Integer.parseInt(tokens[6]));
+                             j <= Math.max(Integer.parseInt(tokens[3]), Integer.parseInt(tokens[6])); j++) {
+                            lights[i][j] = true;
+                        }
+                    }
+                } else if (Objects.equals(tokens[1], "off")) {
+                    for (int i = Math.min(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[5]));
+                         i <= Math.max(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[5])); i++) {
+                        for (int j = Math.min(Integer.parseInt(tokens[3]), Integer.parseInt(tokens[6]));
+                             j <= Math.max(Integer.parseInt(tokens[3]), Integer.parseInt(tokens[6])); j++) {
+                            lights[i][j] = false;
+                        }
+                    }
+                } else {
+                    for (int i = Math.min(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[4]));
+                         i <= Math.max(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[4])); i++) {
+                        for (int j = Math.min(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[5]));
+                             j <= Math.max(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[5])); j++) {
+                            lights[i][j] = !lights[i][j];
+                        }
+                    }
+                }
+            }
+
+            for (boolean[] row : lights) {
+                for (boolean light : row) {
+                    if (light) {
+                        counterLightsOn++;
+                    }
+                }
+            }
+
+            System.out.println(counterLightsOn);
+
+
+            // 444169 too low
+            // 430563 too low
+            // 569999 ok
+
+
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
